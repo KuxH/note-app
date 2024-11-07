5fa8bf613656ae2fd18d6ee44da879a1338614c6
Kushal Neupane <thisiskush7447@gmail.com>
2024-11-07 08:05:34 +0545

    backend

---
+++ note-backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(request, response)=>{
+    response.send('<h1>hello world</h1>')
+})
+
+app.get('/api/notes', (request,response) =>{
+    response.json(notes)
+})
+
+const PORT =3001
+app.listen(PORT, ()=>{
+    console.log('Server running on ${PORT}')
+})
